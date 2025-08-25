9f8bce99e38c776e1e12ad19e99e11352a083910
jumhuur <jumhuur123@hotmail.com>
2025-08-25 20:40:41 +0300

    update

--- xisaab_Kiro.py
+++ xisaab_Kiro.py
@@ -1,7 +1,7 @@
 # xisaabta bishii
 import termcolor
-kiro = [{"Hore": [800,1154], "08/2025":[100,150,200,50,150,150,135,50]}]
-baxay = [170,105,200,20,100]
+kiro = [{"Account": [800,1154], "8":[100,150,200,50,150,150,135,50,165,150]}]
+baxay = [180.50,105,200,20,100,25]
 amaah = [{"Hinda": [10,15,5,3.50,40],"Maxamad": [46], "Maxamad_Yuusuf": [50], "Naasir": [50], "c/shakuur": [200]}]
 def Amaah_le():
     for Amaah in amaah:
@@ -10,20 +10,38 @@ def Amaah_le():
         Tota_Amaah = Hinda + Maxamad;
         print(f"Total Amaah: {Tota_Amaah}")
         return Tota_Amaah
-def Kiro_calc():
-    for k_bil in kiro:
-        Bishii_08 = sum(k_bil["08/2025"])
-        Hore = sum(k_bil["Hore"])
-        return Bishii_08 + Hore
-Total_Kiro = Kiro_calc()
-Total_baxay = sum(baxay)
-print(f'Total Kiro = {Total_Kiro}$')
-print(f"Total Baxay {Total_baxay}$")
+    
+def ChooseMonth():
+    bilo = []
+    print(termcolor.colored("Fadlan Dooro Bisha", "blue"))
+    for number in range(8,9):
+        bilo.append(str(number))
+        print(f"{str(number).zfill(2)} - bisha {number}aad")
+    Bisha = input("Qor bisha ")
+    def Kiro_calc():
+        for k_bil in kiro:
+            Bishii_08 = sum(k_bil[Bisha])
+            Account = sum(k_bil["Account"])
+            return Bishii_08 + Account
+    if Bisha.strip() in bilo:
+            Total_Kiro = Kiro_calc()
+            Total_baxay = sum(baxay)
+            print(f'Total Kiro = {Total_Kiro}$')
+            print(f"Total Baxay {Total_baxay}$")
 
-Total_Guud = Total_Kiro - Total_baxay
-LaHayo = Total_Guud - Amaah_le()
-print(f"Waxaa la doonayay inuu soo hadho :{Total_Guud}$")
-print(f"waxaa maqan {Total_Guud - LaHayo}$")
-print(f"Waxaa la Hayaa {termcolor.colored(LaHayo, "blue")}$")
-# print(f"Main Total: {termcolor.colored(Account_Total + LaHayo, "blue")}$")
+            Total_Guud = Total_Kiro - Total_baxay
+            LaHayo = Total_Guud - Amaah_le()
+            print(f"Waxaa la doonayay inuu soo hadho :{Total_Guud}$")
+            print(f"waxaa maqan {Total_Guud - LaHayo}$")
+            print(f"Total Dhaba {termcolor.colored(LaHayo, "blue")}$")
+    else :
+        print(termcolor.colored("Bisha aad dooratay lama helin", "red"))
+
+ChooseMonth()
+
+
+
+
+
+
 
