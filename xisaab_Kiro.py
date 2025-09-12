46ac2e3b18d738dd43ba1f465cc1b9a6f788acb7
jumhuur <jumhuur123@hotmail.com>
2025-09-12 08:50:52 +0300

    Laptops Bill

--- xisaab_Kiro.py
+++ xisaab_Kiro.py
@@ -1,8 +1,9 @@
 # xisaabta bishii
 import termcolor
-kiro = [{"Account": [2307.50], "8":[100,150,200,50,150,150,135,50,165,150], "9": [300]}]
+kiro = [{"Account": [2307.50], "8":[100,150,200,50,150,150,135,50,165,150], "9": [300, 200,100]}]
 baxay = [{"8":[183.50,105,200,20,100,25,15,150],"9":[130] }]
-amaah = [{"Hinda": [10,15,5,3.50,40],"Maxamad": [46], "Maxamad_Yuusuf": [50], "Naasir": [50], "c/shakuur": [200]}]
+amaah = [{"Hinda": [10,15,5,3.50,40],"Maxamad": [37],  "Naasir": [50], "c/shakuur": [200]}]
+account_out = [650,20,5,8]
 def Amaah_le():
     for Amaah in amaah:
         Hinda = sum(Amaah.get("Hinda"))
@@ -14,9 +15,13 @@ def Baxay(bil):
      for OneBaxay in baxay:
         #   print(OneBaxay.get(bil))
           return OneBaxay.get(bil)
+def Account_out_bill():
+    #  for cash in account_out:
+    #       return sum(cash)
+    return sum(account_out)
 def ChooseMonth():
     bilo = []
-    print(termcolor.colored("Fadlan Dooro Bisha".title(), "blue".title()))
+    print(termcolor.colored("Fadlan Dooro Bisha".title(), "blue"))
     for number in range(8,10):
         bilo.append(str(number))
         print(f"{str(number).zfill(2)} - bisha {number}aad".title())
@@ -38,8 +43,7 @@ def ChooseMonth():
     elif Bisha.strip().upper() == "A":
         for Account in kiro:
              Total_Account = sum(Account.get("Account"))
-             print(Total_Account)
-            # print(Account = sum(k_bil["Account"]))
+             print(Total_Account - Account_out_bill())
     else :
         print(termcolor.colored("Si sax waxba umaad dooran".title(), "red"))
 ChooseMonth()
