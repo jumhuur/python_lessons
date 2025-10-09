ea0fb261de457da1edf05e52d49836d95342a534
jumhuur <jumhuur123@hotmail.com>
2025-10-09 21:53:23 +0300

    un comment

--- Week 07/built_in_function.py
+++ Week 07/built_in_function.py
@@ -1,24 +1,24 @@
-# # map
+# map
 
-# # Example 01
-# from functools import reduce
-
-
-# def formater(text):
-#     return f"--{text.strip().capitalize()}--"
-# texts = ["cimraan  " , "cabdilaahi" , "  ciise"]
-# formated = map(formater, texts)
-
-# for text in formated:
-#     print(text)
+# Example 01
+from functools import reduce
 
 
+def formater(text):
+    return f"--{text.strip().capitalize()}--"
+texts = ["cimraan  " , "cabdilaahi" , "  ciise"]
+formated = map(formater, texts)
 
-# # Example 02
-# items = [["ss", "aa", 2020], {"name": "maxamad"}, 45, 77, 1.22,25.22, "nimcaan", (1,2,3)]
-# check = map(lambda item : f"{item} type is {type(item)}",items)
-# for ck in check:
-#     print(ck)
+for text in formated:
+    print(text)
+
+
+
+# Example 02
+items = [["ss", "aa", 2020], {"name": "maxamad"}, 45, 77, 1.22,25.22, "nimcaan", (1,2,3)]
+check = map(lambda item : f"{item} type is {type(item)}",items)
+for ck in check:
+    print(ck)
 
 
 
@@ -35,29 +35,29 @@ print(data)
 for result in  data:
     print(result)
 
-# # Example 2
-# small = [41,11,2,45,78,96,63,100,451,1]
-# print("*" * 30)
-# data = filter(lambda num : num + num <= 100, small)
-# for result in  data:
-#     print(result)
+# Example 2
+small = [41,11,2,45,78,96,63,100,451,1]
+print("*" * 30)
+data = filter(lambda num : num + num <= 100, small)
+for result in  data:
+    print(result)
 
-# print("*" * 35)
-# listall = ["a", "12", "wew", 45,77,10,3.23]
-# for one in list(filter(lambda item : type(item) == int or  type(item) == float, listall)):
-#     print(one)
+print("*" * 35)
+listall = ["a", "12", "wew", 45,77,10,3.23]
+for one in list(filter(lambda item : type(item) == int or  type(item) == float, listall)):
+    print(one)
 
 
-# print("*" * 35)
-# # reduce
+print("*" * 35)
+# reduce
 
-# # Example 01
+# Example 01
 
-# allnums = [44,2.50,44.23,25.12,2.02,45.55,14.78,45.98]
+allnums = [44,2.50,44.23,25.12,2.02,45.55,14.78,45.98]
 
-# def sumall(num1, num2):
-#     return num1 + num2
+def sumall(num1, num2):
+    return num1 + num2
 
-# reduced = reduce(sumall, allnums)
-# print(round(reduced,5))
-# print(reduced)
+reduced = reduce(sumall, allnums)
+print(round(reduced,5))
+print(reduced)
