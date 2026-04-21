7227a7487928cdac947bfb755306dc4774adde3d
jumhuur <jumhuur123@hotmail.com>
2026-04-21 11:16:01 +0300

    week 01 update

--- Week 01/escape_sequence.py
+++ Week 01/escape_sequence.py
@@ -1,47 +1,34 @@
-#--------------------------
-# \b = waxaa ma saxayaa kaliya waxii ka horeeyay hal matr waa backspace
-#\ Error
-# \\  waxay ka boodaysaa \
-#\'' ka bood ''
-#\ "" ka bood ""
-#\n line feed
-#\r carige return
-#\t Horizontal Tab
-#\xhh  character hex value
-#--------------------------
+# python lessons
+# ---------------------------------------
+# \b = backspace
+# \ = skip new line in str
+# \\ = skip back slash
+# \' = skip single qoute('')
+# \" = skip double qoutes("")
+# \n = new line
+# \r carrige return
+# \x = charagter hjex value
+# ----------------------------------------
 
 
-#\b 
-print("hell\bo python") # helo python
+# \b backspace
+print("soo dhawaad\ba dhamaantiin n\b")
+# \ skip new line
+print(
+    "waxaan \
+salaamaya \
+dhamaan ardayda"
+)
+# \\ skip backslash
+print("this is my backslash \\")
+# \' skip double qoutes
+print('waxaan ahay maxamad caan ku ah "jumhuur"')
+# \' skip single qoutes
+print("waxaan ahay maxamad caan ku ah 'jumhuur'")
 
-#\ iima shaqayn 
-# print("waxaan jeclahay \
-# luuqada\
-# waayo waa luuqada\
-# fudud sidoo kale\ 
-# sahlan\
-# ")
-
-# \\ 
-print("tani waa slash \\") # tani waa slash \
-
-
-#\""
-print("kani waa \'single\'") # kani waa 'single'
-
-#\""
-print("kani waa \"Laba\"") # kani waa "Laba
-
-
-#\n
-print("Magacagu waa : \n maxamad \n waxan ahay : \nFull stack eng") # sadex layn
-
-#\r
-print("1234\rabcd") # abcd !
-
-#\t
-print("we love \tpython") #we love         python
-#\xhh
-print("kani waa \x41") #kani waa A
-print("kani waa \x42") #kani waa B
-print("kani waa \x50") #kani waa P
+# \n  new line
+print("waxaad tahay \narday \nwanagsan \nnaxariis badan \ndadaalaya")
+#  \r carrige return
+print("1233\rabc")
+# \x hex value
+print("\x56\x4b"
